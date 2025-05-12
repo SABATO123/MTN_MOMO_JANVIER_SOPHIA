52d5ac6dcb2eff6bc4d185edfd102de433b8a92a
Mukarukundo-Sophie <164475120+Mukarukundo-Sophie@users.noreply.github.com>
2025-05-12 14:25:25 +0300

    commited

--- App/util.php
+++ App/util.php
@@ -8,7 +8,6 @@ class Util {
     static $db = 'momo_apps'; 
     static $user = 'root';          
     static $pass = '';   
-    
     static $username="sandbox";
     static $apikey = "atsk_8066c257f806f88e6a7793ff33228d0110ed34ccd6389a112b24fd13f0be981dde2f2313";
     static $Company = "JANVIER LTD";
